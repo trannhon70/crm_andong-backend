b3de8c0be8c0438758ea9ab753836b7b8509b8ae
trannhon70 <kevintran351996@gmail.com>
2024-11-11 14:05:18 +0700

    update

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Req } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req } from "@nestjs/common";
 import { MediaService } from "./media.service";
 
 
@@ -20,12 +20,62 @@ export class MediaController {
     }
 
     @Get('get-all/:id')
-    async getpaging(@Param('id') id: number) {
+    async getall(@Param('id') id: number) {
         const data = await this.mediasService.getall(id);
         return {
             statusCode: 1,
-            message: 'get paging all success',
+            message: 'get all success',
             data: data,
         };
+    }
+
+    @Get('get-paging')
+     async getpaging(@Req() req: any, @Query() query: any){
+        const data = await this.mediasService.getpaging(req, query);
+        return {
+            statusCode: 1,
+            message: 'get paging media success',
+            data: data,
+        };
+    }
+
+    @Get('get-by-id/:id')
+     async getById(@Param('id') id: number){
+        const data = await this.mediasService.getById(id);
+        return {
+            statusCode: 1,
+            message: 'get media by id success!',
+            data: data,
+        };
+    }
+
+    @Put('update/:id')
+    async update(@Param('id') id: number, @Body() body: any) {
+        try {
+                const data = await this.mediasService.update(id, body)
+                return {
+                    statusCode: 1,
+                    message: 'update role success!',
+                    data: data
+                }
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+    @Delete('delete/:id')
+    async delete(@Param('id') id: number){
+        try {
+            const data = await this.mediasService.delete(id);
+            return {
+                statusCode: 1,
+                message: 'delete media success!',
+                data: data
+            }
+        } catch (error) {
+            console.log(error);
+            
+        }
     }
 }
--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -3,6 +3,7 @@ import { Media } from "./media.entity";
 import { Repository } from "typeorm";
 import { JwtService } from "@nestjs/jwt";
 import { currentTimestamp } from "utils/currentTimestamp";
+import { NotFoundException } from "@nestjs/common";
 
 
 export class MediaService {
@@ -23,6 +24,14 @@ export class MediaService {
         const decoded = await this.jwtService.verify(token); 
         const userId = decoded.id;
 
+        const check = await this.mediaRepository.findOne({
+            where: {name: body.name, hospitalId : body.hospitalId}
+        });
+
+        if(check) {
+            throw new NotFoundException('Media này đã tồn tại!');
+        }
+
         const data : any = {
             name: body.name,
             userId: userId,
@@ -38,5 +47,72 @@ export class MediaService {
             where: {hospitalId: id}
         });
         return result;
+    }
+
+    async getpaging(req: any, query: any) {
+        const pageIndex = query.pageIndex ? parseInt(query.pageIndex, 10) : 1;
+        const pageSize = query.pageSize ? parseInt(query.pageSize, 10) : 10;
+        const search = query.search ? query.search.trim() : '';
+        const hospitalId = query.hospitalId || 0;
+        const skip = (pageIndex - 1) * pageSize;
+
+        let whereCondition = '';
+        const parameters: any = {};
+
+        if (hospitalId !== 0) {
+            whereCondition += 'media.hospitalId = :hospitalId'; 
+            parameters.hospitalId = hospitalId;
+        }
+
+        if (search) {
+            if (whereCondition) whereCondition += ' AND ';
+            whereCondition += '(media.name LIKE :search)';
+            parameters.search = `%${search}%`;
+        }
+
+        const qb = this.mediaRepository.createQueryBuilder('media')
+            .skip(skip)
+            .take(pageSize)
+            .orderBy('media.id', 'DESC');
+        if (whereCondition) {
+            qb.where(whereCondition, parameters);
+        }
+
+        const [result, total] = await qb.getManyAndCount();
+        return {
+            data: result,
+            total: total,
+            pageIndex: pageIndex,
+            pageSize: pageSize,
+            totalPages: Math.ceil(total / pageSize),
+        };
+    }
+
+    async getById (id: number) {
+        if(id){
+            const result = await this.mediaRepository.findOne({
+                where: {id}
+            })
+            return result
+        }
+    }
+
+    async update (id: number, body:any) {
+        const media = await this.mediaRepository.findOne({
+            where:{id}
+        }); 
+
+        if (!media) {
+            throw new Error('Media not found');
+        }
+
+        Object.assign(media, body);
+        return await this.mediaRepository.save(media);
+    }
+
+    async delete (id: number) {
+        if(id){
+            return this.mediaRepository.delete(id)
+        }
     }
 }
